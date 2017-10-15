2414c7683948423a150c26f4b09bd67b9ef7c4d0
Gerhard Poul <gerhard.poul@gmail.com>
2017-10-15 20:23:02 +0200

    Allow underscore '_' in field identifiers within Packages files

--- lib/debian/debian_support.py
+++ lib/debian/debian_support.py
@@ -326,7 +326,7 @@ class PackageFile:
     Objects of this class can be used to read Debian's Source and
     Packages files."""
 
-    re_field = re.compile(r'^([A-Za-z][A-Za-z0-9-]+):(?:\s*(.*?))?\s*$')
+    re_field = re.compile(r'^([A-Za-z][A-Za-z0-9-_]+):(?:\s*(.*?))?\s*$')
     re_continuation = re.compile(r'^\s+(?:\.|(\S.*?)\s*)$')
 
     def __init__(self, name, file_obj=None):
