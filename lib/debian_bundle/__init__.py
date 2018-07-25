2d80fd6ac90b4ccfda54ba7ccca83609ec96f801
Stuart Prescott <stuart@debian.org>
2018-07-25 16:03:51 +0000

    Type annotations for debian_bundle

--- lib/debian_bundle/__init__.py
+++ lib/debian_bundle/__init__.py
@@ -7,5 +7,5 @@ warnings.warn("please use 'debian' instead of 'debian_bundle'",
               DeprecationWarning, stacklevel=2)
 
 # Support "from debian_bundle import foo"
-parent_dir = os.path.dirname(__path__[0])
-__path__.append(os.path.join(parent_dir, "debian"))
+parent_dir = os.path.dirname(__path__[0])  # type: ignore  # mypy issue #1422
+__path__.append(os.path.join(parent_dir, "debian"))  # type: ignore
