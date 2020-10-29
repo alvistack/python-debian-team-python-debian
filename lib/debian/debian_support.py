804cda852379e99746a2d92678f1e5948c5e38b5
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-10-29 20:36:49 +0100

    Support for StringIO as input file

--- lib/debian/debian_support.py
+++ lib/debian/debian_support.py
@@ -418,7 +418,7 @@ class PackageFile:
 
     def __iter__(self):
         # type: () -> Generator[Iterable[Tuple[str, str]], None, None]
-        line = self.file.readline().decode(self.encoding)
+        line = self._aux_read_line()
         self.lineno += 1
         pkg = []   # type: List[Tuple[str, str]]
         while line:
@@ -427,7 +427,7 @@ class PackageFile:
                     self.raise_syntax_error('expected package record')
                 yield pkg
                 pkg = []
-                line = self.file.readline().decode(self.encoding)
+                line = self._aux_read_line()
                 self.lineno += 1
                 continue
 
@@ -438,7 +438,7 @@ class PackageFile:
             contents = contents or ''
 
             while True:
-                line = self.file.readline().decode(self.encoding)
+                line = self._aux_read_line()
                 self.lineno += 1
                 match = self.re_continuation.match(line)
                 if match:
@@ -451,6 +451,14 @@ class PackageFile:
             pkg.append((name, contents))
         if pkg:
             yield pkg
+
+    def _aux_read_line(self) -> str:
+        # Not always readline returns a byte object, also str
+        # can be returned (i.e: StringIO)
+        try:
+            return self.file.readline().decode(self.encoding)
+        except AttributeError:
+            return self.file.readline()
 
     def raise_syntax_error(self, msg, lineno=None):
         # type: (str, Optional[int]) -> NoReturn
