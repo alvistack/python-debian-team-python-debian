f96250184d76e8a113fdae50b8f38aed3fd2c532
Niels Thykier <niels@thykier.net>
2021-08-14 17:00:31 +0000

    Add Doc-string to AmbiguousDeb822FieldKeyError
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/debian/_deb822_repro.py
+++ lib/debian/_deb822_repro.py
@@ -135,7 +135,21 @@ _RE_FIELD_LINE = re.compile(r'''
 
 
 class AmbiguousDeb822FieldKeyError(KeyError):
-    pass
+    """Specialized version of KeyError to denote a valid but ambiguous field name
+
+    This exception occurs if:
+      * the field is accessed via a str on a configured view that does not automatically
+        resolve ambiguous field names (see Deb822ParagraphElement.configured_view), AND
+      * a concrete paragraph contents a repeated field (which is not valid in deb822
+        but the module supports parsing them)
+
+    Note that the default is to automatically resolve ambiguous fields. Accordingly
+    you will only see this exception if you have "opted in" on wanting to know that
+    the lookup was ambiguous.
+
+    The ambiguity can be resolved by using a tuple of (<field-name>, <filed-index>)
+    instead of <field-name>.
+    """
 
 
 def _resolve_ref(ref: Optional[ReferenceType[T]]) -> Optional[T]:
