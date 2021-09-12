40d3a52384faf38c7c1c2b77196923ab9fff1d8f
Niels Thykier <niels@thykier.net>
2021-09-12 15:59:37 +0000

    Make types.py python3.5 compatiable
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- lib/debian/_deb822_repro/types.py
+++ lib/debian/_deb822_repro/types.py
@@ -1,59 +1,61 @@
-from typing import TypeVar, Union, Tuple, List, TYPE_CHECKING
+try:
+    from typing import TypeVar, Union, Tuple, List, TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from debian._deb822_repro.tokens import Deb822Token, Deb822FieldNameToken
-    from debian._deb822_repro.parsing import Deb822Element, Deb822CommentElement
+    if TYPE_CHECKING:
+        from debian._deb822_repro.tokens import Deb822Token, Deb822FieldNameToken
+        from debian._deb822_repro.parsing import Deb822Element, Deb822CommentElement
 
+    # Used a generic type for any case where we need a generic type without any bounds
+    # (e.g. for the LinkedList interface and some super-classes/mixins).
+    T = TypeVar('T')
+    T.__doc__ = """
+    Generic type
+    """
 
-# Used a generic type for any case where we need a generic type without any bounds
-# (e.g. for the LinkedList interface and some super-classes/mixins).
-T = TypeVar('T')
-T.__doc__ = """
-Generic type
-"""
+    TokenOrElement = Union['Deb822Element', 'Deb822Token']
+    TE = TypeVar('TE', bound=TokenOrElement)
+    TE.__doc__ = """
+    Generic "Token or Element" type
+    """
 
-TokenOrElement = Union['Deb822Element', 'Deb822Token']
-TE = TypeVar('TE', bound=TokenOrElement)
-TE.__doc__ = """
-Generic "Token or Element" type
-"""
+    # Used as a resulting element for "mapping" functions that map TE -> R (see _combine_parts)
+    R = TypeVar('R', bound='Deb822Element')
+    R.__doc__ = """
+    For internal usage in _deb822_repro
+    """
 
-# Used as a resulting element for "mapping" functions that map TE -> R (see _combine_parts)
-R = TypeVar('R', bound='Deb822Element')
-R.__doc__ = """
-For internal usage in _deb822_repro
-"""
+    VT = TypeVar('VT', bound='Deb822Token')
+    VT.__doc__ = """
+    Value type/token in a list interpretation of a field value
+    """
 
-VT = TypeVar('VT', bound='Deb822Token')
-VT.__doc__ = """
-Value type/token in a list interpretation of a field value
-"""
+    ST = TypeVar('ST', bound='Deb822Token')
+    ST.__doc__ = """
+    Separator type/token in a list interpretation of a field value
+    """
 
-ST = TypeVar('ST', bound='Deb822Token')
-ST.__doc__ = """
-Separator type/token in a list interpretation of a field value
-"""
+    # Internal type for part of the paragraph key.  Used to facility _unpack_key.
+    ParagraphKeyBase = Union['Deb822FieldNameToken', str]
+    ParagraphKeyBase.__doc__ = """
+    For internal usage in _deb822_repro
+    """
 
-# Internal type for part of the paragraph key.  Used to facility _unpack_key.
-ParagraphKeyBase = Union['Deb822FieldNameToken', str]
-ParagraphKeyBase.__doc__ = """
-For internal usage in _deb822_repro
-"""
+    ParagraphKey = Union[ParagraphKeyBase, Tuple[str, int]]
+    ParagraphKey.__doc__ = """
+    Anything accepted as a key for a paragraph field lookup.  The simple case being
+    a str. Alternative variants are mostly interesting for paragraphs with repeated
+    fields (to enable unambiguous lookups)
+    """
 
-ParagraphKey = Union[ParagraphKeyBase, Tuple[str, int]]
-ParagraphKey.__doc__ = """
-Anything accepted as a key for a paragraph field lookup.  The simple case being
-a str. Alternative variants are mostly interesting for paragraphs with repeated
-fields (to enable unambiguous lookups)
-"""
-
-Commentish = Union[List[str], 'Deb822CommentElement']
-Commentish.__doc__ = """
-Anything accepted as input for a Comment. The simple case is the list
-of string (each element being a line of comment). The alternative format is
-there for enable reuse of an existing element (e.g. to avoid "unpacking"
-only to "re-pack" an existing comment element).
-"""
+    Commentish = Union[List[str], 'Deb822CommentElement']
+    Commentish.__doc__ = """
+    Anything accepted as input for a Comment. The simple case is the list
+    of string (each element being a line of comment). The alternative format is
+    there for enable reuse of an existing element (e.g. to avoid "unpacking"
+    only to "re-pack" an existing comment element).
+    """
+except ImportError:
+    pass
 
 
 class AmbiguousDeb822FieldKeyError(KeyError):
