b784c41791552b6508f27e0a22e9ba1b2902b66f
Stuart Prescott <stuart@debian.org>
2022-11-04 12:58:42 +0000

    Convert to asserts test_arch_table.py

--- lib/debian/tests/test_arch_table.py
+++ lib/debian/tests/test_arch_table.py
@@ -35,83 +35,83 @@ class TestDpkgArchTable(unittest.TestCase):
     def test_matches_architecture(self):
         # type: () -> None
         arch_table = StubbedDpkgArchTable.load_arch_table()
-        self.assertTrue(arch_table.matches_architecture("amd64", "linux-any"))
-        self.assertTrue(arch_table.matches_architecture("i386", "linux-any"))
-        self.assertTrue(arch_table.matches_architecture("amd64", "amd64"))
+        assert arch_table.matches_architecture("amd64", "linux-any")
+        assert arch_table.matches_architecture("i386", "linux-any")
+        assert arch_table.matches_architecture("amd64", "amd64")
 
-        self.assertFalse(arch_table.matches_architecture("i386", "amd64"))
-        self.assertFalse(arch_table.matches_architecture("all", "amd64"))
+        assert not arch_table.matches_architecture("i386", "amd64")
+        assert not arch_table.matches_architecture("all", "amd64")
 
-        self.assertTrue(arch_table.matches_architecture("all", "all"))
+        assert arch_table.matches_architecture("all", "all")
 
         # i386 is the short form of linux-i386. Therefore, it does not match kfreebsd-i386
-        self.assertFalse(arch_table.matches_architecture("i386", "kfreebsd-i386"))
+        assert not arch_table.matches_architecture("i386", "kfreebsd-i386")
 
         # Note that "armel" and "armhf" are "arm" CPUs, so it is matched by "any-arm"
         # (similar holds for some other architecture <-> CPU name combinations)
         for n in ['armel', 'armhf']:
-            self.assertTrue(arch_table.matches_architecture(n, 'any-arm'))
+            assert arch_table.matches_architecture(n, 'any-arm')
         # Since "armel" is not a valid CPU name, this returns False (the correct would be
         # any-arm as noted above)
-        self.assertFalse(arch_table.matches_architecture("armel", "any-armel"))
+        assert not arch_table.matches_architecture("armel", "any-armel")
 
         # Wildcards used as architecture always fail (except for special cases noted in the
         # compatibility notes below)
-        self.assertFalse(arch_table.matches_architecture("any-i386", "i386"))
+        assert not arch_table.matches_architecture("any-i386", "i386")
 
         # any-i386 is not a subset of linux-any (they only have i386/linux-i386 as overlap)
-        self.assertFalse(arch_table.matches_architecture("any-i386", "linux-any"))
+        assert not arch_table.matches_architecture("any-i386", "linux-any")
 
         # Compatibility with dpkg - if alias is `any` then it always returns True
         # even if the input otherwise would not make sense.
-        self.assertTrue(arch_table.matches_architecture("any-unknown", "any"))
+        assert arch_table.matches_architecture("any-unknown", "any")
         # Another side effect of the dpkg compatibility
-        self.assertTrue(arch_table.matches_architecture("all", "any"))
+        assert arch_table.matches_architecture("all", "any")
 
         # STUB VERIFICATION: This would return True if we used real data.  But we are supposed to
         # use the stub which does not have data for this architecture.
         # (If this fails because you added the architecture to the stub, then replace it with
         # another architecture, so the verification still works)
-        self.assertFalse(arch_table.matches_architecture('mipsel', 'any-mipsel'))
+        assert not arch_table.matches_architecture('mipsel', 'any-mipsel')
 
     def test_arch_equals(self):
         # type: () -> None
         arch_table = StubbedDpkgArchTable.load_arch_table()
-        self.assertTrue(arch_table.architecture_equals("linux-amd64", "amd64"))
-        self.assertFalse(arch_table.architecture_equals("amd64", "linux-i386"))
-        self.assertFalse(arch_table.architecture_equals("i386", "linux-amd64"))
-        self.assertTrue(arch_table.architecture_equals("amd64", "amd64"))
-        self.assertFalse(arch_table.architecture_equals("i386", "amd64"))
+        assert arch_table.architecture_equals("linux-amd64", "amd64")
+        assert not arch_table.architecture_equals("amd64", "linux-i386")
+        assert not arch_table.architecture_equals("i386", "linux-amd64")
+        assert arch_table.architecture_equals("amd64", "amd64")
+        assert not arch_table.architecture_equals("i386", "amd64")
 
         # Compatibility with dpkg: if the parameters are equal, then it always return True
-        self.assertTrue(arch_table.architecture_equals("unknown", "unknown"))
+        assert arch_table.architecture_equals("unknown", "unknown")
 
     def test_architecture_is_concerned(self):
         # type: () -> None
         arch_table = StubbedDpkgArchTable.load_arch_table()
-        self.assertTrue(arch_table.architecture_is_concerned("linux-amd64", ["amd64", "i386"]))
-        self.assertFalse(arch_table.architecture_is_concerned("amd64", ["!amd64", "!i386"]))
+        assert arch_table.architecture_is_concerned("linux-amd64", ["amd64", "i386"])
+        assert not arch_table.architecture_is_concerned("amd64", ["!amd64", "!i386"])
         # This is False because the "!amd64" is matched first.
-        self.assertFalse(arch_table.architecture_is_concerned(
+        assert not arch_table.architecture_is_concerned(
             "linux-amd64",
             ["!linux-amd64", "linux-any"],
             allow_mixing_positive_and_negative=True
-        ))
+        )
         # This is True because the "linux-any" is matched first.
-        self.assertTrue(arch_table.architecture_is_concerned(
+        assert arch_table.architecture_is_concerned(
             "linux-amd64",
             ["linux-any", "!linux-amd64"],
             allow_mixing_positive_and_negative=True
-        ))
+        )
 
     def test_is_wildcard(self):
         # type: () -> None
         arch_table = StubbedDpkgArchTable.load_arch_table()
-        self.assertTrue(arch_table.is_wildcard("linux-any"))
-        self.assertFalse(arch_table.is_wildcard("amd64"))
-        self.assertFalse(arch_table.is_wildcard("unknown"))
+        assert arch_table.is_wildcard("linux-any")
+        assert not arch_table.is_wildcard("amd64")
+        assert not arch_table.is_wildcard("unknown")
         # Compatibility with the dpkg version of the function.
-        self.assertTrue(arch_table.is_wildcard("unknown-any"))
+        assert arch_table.is_wildcard("unknown-any")
 
     @skipIf(not HAS_REAL_DATA, "Missing real data")
     def test_has_real_data(self):
@@ -120,4 +120,4 @@ class TestDpkgArchTable(unittest.TestCase):
         # The tests here rely on the production data, so we can use mips (which is not present in
         # our stubbed data).
 
-        self.assertTrue(arch_table.matches_architecture('mipsel', 'any-mipsel'))
+        assert arch_table.matches_architecture('mipsel', 'any-mipsel')
