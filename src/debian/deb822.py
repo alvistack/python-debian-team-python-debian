4e7f9387c16cfbdce978710e86cf9b5cdfdf2cfd
Alexandre Detiste <alexandre.detiste@gmail.com>
2024-03-01 09:35:27 +0100

    remove some Python2 dead code

--- src/debian/deb822.py
+++ src/debian/deb822.py
@@ -841,18 +841,12 @@ class Deb822(Deb822Dict):
         if curkey:
             self[curkey] = content
 
-    def __str__(self):
+    def __str__(self) -> str:
         # type: () -> str
         d = self.dump()
         return d if d is not None else ""
 
-    def __unicode__(self):
-        # type: () -> str
-        d = self.dump()
-        return d if d is not None else ""
-
-    def __bytes__(self):
-        # type: () -> bytes
+    def __bytes__(self) -> bytes:
         d = self.dump()
         return d.encode(self.encoding) if d is not None else b""
 
