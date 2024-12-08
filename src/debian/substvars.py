52481d025a9b4d23780d6f0bdecbd27971bda9b1
Stuart Prescott <stuart@debian.org>
2024-12-08 12:41:39 +1100

    Fix typo in docstring

--- src/debian/substvars.py
+++ src/debian/substvars.py
@@ -1,8 +1,8 @@
 """ Facilities for reading and writing Debian substvars files
 
 The aim of this module is to provide programmatic access to Debian substvars
-files to query and manipulate them. The format for the changelog is defined in
-`deb-substvars(5)
+files to query and manipulate them. The format for the substvars files is
+defined in `deb-substvars(5)
 <https://manpages.debian.org/unstable/dpkg-dev/deb-substvars.5.html>`_
 
 Overview
